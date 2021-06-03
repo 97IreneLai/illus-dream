9f1e0e8cb64eba1d7f55b6670913f53317da4da4
Irene_Lai97 <61180@siswa.unimas.my>
2021-06-03 12:35:22 +0800

    changes in button

--- resources/js/pages/MyIllustration.vue
+++ resources/js/pages/MyIllustration.vue
@@ -10,7 +10,7 @@
                     </div>
                     <div class="text-center card-overlay">
                         <div>
-                            <button type="button" class="dropdown-toggle dropdown-toggle-split text" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <button type="button" class="dropdown-toggle dropdown-toggle-split text4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <!-- <span class="sr-only">Toggle Dropdown</span> -->
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="13" viewBox="0 0 56.67 16">
                                     <path id="FontAwsome_ellipsis-h_" data-name="FontAwsome (ellipsis-h)" d="M44.561,192a8.229,8.229,0,1,1-8.226-8A8.11,8.11,0,0,1,44.561,192Zm11.882-8a8,8,0,1,0,8.226,8A8.11,8.11,0,0,0,56.444,184Zm-40.217,0a8,8,0,1,0,0,16,8,8,0,1,0,0-16Z" transform="translate(-8 -184)" fill="#284e5b"/>
@@ -184,6 +184,20 @@ export default {
         position: absolute;
         top: 50%;
         left: 50%;
+        transform: translate(-50%, -50%);
+        margin: 0;
+    }
+
+    .text4{
+        font-family: $primary-font;
+        font-size: 20px;
+        font-weight: 500;
+        color: rgb(173, 173, 173);
+        letter-spacing: 1px;
+        text-align: center;
+        position: absolute;
+        top: 15%;
+        left: 80%;
         transform: translate(-50%, -50%);
         margin: 0;
     }
